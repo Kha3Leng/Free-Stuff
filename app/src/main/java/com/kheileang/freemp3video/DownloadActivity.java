912cff65facecb502b8e2503786e42969989c1cf
Thura <saikhae8@gmail.com>
2021-06-18 11:28:25 +0630

    adding viewpager tab layout

--- app/src/main/java/com/kheileang/freemp3video/DownloadActivity.java
+++ app/src/main/java/com/kheileang/freemp3video/DownloadActivity.java
@@ -1,14 +1,49 @@
 package com.kheileang.freemp3video;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class DownloadActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+
+        tabLayout = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.pager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("MP3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Video"));
+
+        TabAdapter adapter = new TabAdapter(getSupportFragmentManager(), 1, this, tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/kheileang/freemp3video/TabAdapter.java
@@ -0,0 +1,36 @@
+package com.kheileang.freemp3video;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+    Context context;
+    int tabLen;
+
+    public TabAdapter(@NonNull FragmentManager fm, int behavior, Context context, int tabLen) {
+        super(fm, behavior);
+        this.context = context;
+        this.tabLen = tabLen;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+            case 1:
+                return new mp3Fragment();
+            default:
+                return new mp3Fragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabLen;
+    }
+}
